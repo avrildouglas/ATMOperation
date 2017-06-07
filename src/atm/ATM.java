3cb6cd585646d46945a8ac98d01ba8c297c15ca0
Avril <braddoug76@yahoo.com>
2017-06-07 08:24:39 -0400

    second commit

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -9,39 +9,69 @@ public class ATM {
 	ATMApplication accATMApp = new ATMApplication();
 	
 	Account accAccount = new Account();
+	static double oldBalance, newBalance, deposit, withdraw,
+	static amtCurrBal;
+
+	static double oldBalance, newBalance, deposit, withdraw;
+	//static int choose;
+	static int accI=0;
+
+	public static String validateAccount(String accNum, int pinNum) {
+		
+		String msgResult = "The account info you entered is invalid.";
+		
+		//declare arrays
+		String accNumber[] = {"059271-10", "078432-11", "093647-12"};
+		int pinNumber[] = {4011, 2810, 9764};
+		
+		for(accI=0; accI < accNumber.length; accI++){
+			String resultAcc = accNumber[accI];
+		    int resultPin = pinNumber[accI];
+		    if (accNum == resultAcc && pinNum == resultPin) {
+		        msgResult = " ";
+		  //  } else{
+		   // 	msgResult = "The account info you entered is invalid"; 
+		       } 
+			}
+		return msgResult;
+	}
 	
-	ArrayList<Integer> accNumber = new ArrayList<Integer>();
-	ArrayList<Integer> pinNumber = new ArrayList<Integer>();
-   
-	int accI=0;
-
-
-	public static boolean[] validateAccAccount(int accnumber) {
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		percent.setMaximumFractionDigits(2);
-		String majorityString = percent.format(majority);
-		return majorityString;
-	}
-	public static boolean[] validateAccPin(int accpin) {
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		percent.setMaximumFractionDigits(2);
-		String majorityString = percent.format(majority);
-		return majorityString;
+	public static void makeDeposit() {
+		double amtDeposit;
+		
+		Scanner inputDeposit = new Scanner(System.in);
+		System.out.println("Enter the deposit amount:");
+		amtDeposit = inputDeposit.nextDouble();
+		System.out.println("You have deposited: " + amtDeposit);
+		amtCurrBal += amtDeposit;
+		System.out.println("Your account balance is: " + amtCurrBal);
 	}
 	
-	public static int Deposits. (int accDeposit) {
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		percent.setMaximumFractionDigits(2);
-		String majorityString = percent.format(majority);
-		return majorityString;
+	public static void makeWithdrawal() {
+		double amtWithdrawal;
+		
+	   Scanner inputWithdrawal = new Scanner(System.in);
+	   System.out.println("Enter the withdrawal amount: ");
+	   amtWithdrawal = inputWithdrawal.nextDouble();
+	   System.out.println("Your withdrawal amount: " + amtWithdrawal);
+	   amtcurrBal -= amtWithdrawal;
+	   System.out.println("Your new balance is: " + amtAfterWithdraw);
 	}
 	
-	public static int Withdrawals(int accDeposit) {
+	public static double showBalance() {
+		double prevBalance;
+	    System.out.println("Your current balance is: $" + CurrBalance);
+	    prevBalance = 0.00;
+	    return 1;
+	}
+		
+	
+	//NEED TO ADD DEPPOSITS, WITHDRAWALS, BALANCE, OTHER TRANSACTION
+	/**	public static int Deposits (double accDeposit) {
 		NumberFormat percent = NumberFormat.getPercentInstance();
 		percent.setMaximumFractionDigits(2);
 		String majorityString = percent.format(majority);
-		return majorityString;
-	}
-
-
+		return accDeposit; */
 }
+
+
--- src/atm/ATMApplication.java
+++ src/atm/ATMApplication.java
@@ -6,47 +6,60 @@ package atm;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ATMApplication {
 
+/**public class ATM {
+static Scanner keyboard = new Scanner(System.in);
+static String acctNum, pwd, result;
+static double oldBalance, newBalance, deposit, withdraw;
+static int choose;*/
+
+public class ATMApplication {
+	//static
+	static Scanner inputAcctInfo = new Scanner(System.in);
+	static String numAccount;
+	static int numPin;
+	static String outcome;
+		
+	//Calls the ATM class
+	ATM accATM = new ATM();
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		//Calls the ATM class
-		ATM accATM = new ATM();
-		
-	     int numAccount;
-	     int numPin;
-	     boolean valid = false;
-	     
-	     //get user account number & pin number
-	     do{
- 
-	    	 System.out.println("Enter your account number: ");
-	    	 Scanner inputNumAcct = new Scanner(System.in); 
-	    	 numAccount = inputNumAcct.nextInt();
-	    	 if (numAccount == accATM.{ 
-	    		valid = true;
-	    	 else
-	    		 valid = false
-	     }while (valid = false);
-
-	     do{
-	    	 
-	     Scanner inputNumPin = new Scanner(System.in);  
-		 System.out.println("Enter your pin number: ");
-		 numPin = inputNumPin.nextInt();
-	   	 if (numPin) == get
-		    		valid = true
-		    	 else
-		    		 valid = false
-		     }while (valid = false);
+		//get user account number & pin number
+		for (int chkAn = 0; chkAn < 3; chkAn++) {
+		    System.out.println("Enter your account number: ");
+		    numAccount = inputAcctInfo.nextLine();
+		    System.out.println("Enter your four digit pin: ");
+		    numPin = inputAcctInfo.nextInt();
+		    outcome = 
+		    		//validateAccount(numAccount, numPin);
+		    if (!outcome.equals("The account info you entered is invalid.")) {
+		        break;
+		    //3 attempts allowed
+		    } else if (chkAn == 2) {
+		     System.out.println("You are only allowed 3 attempts!! Account locked!! Call technical support!!");
+		        return;
+		  }    
+		    
+	
+	// Need to ADD OUTPUT results
+		    
+		    
 		 //output
 		 
-		System.out.println("The User name  is: " + acc.getUserName());
-		System.out.println("Account number is: " + acc.getAccNumber());
-		System.out.println("Acount balance is: " + acc.getAccBalance());
+	//	System.out.println("The User name  is: " + acc.getUserName());
+	//	System.out.println("Account number is: " + acc.getAccNumber());
+	//	System.out.println("Acount balance is: " + acc.getAccBalance());
  
 	
+
+
+			    }
+		
+		
+		
 	 
 		 
 		 
